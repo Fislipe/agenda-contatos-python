609daf2a37865d4596a205a8b87f2d699e1a8a33
FILIPE NUNES <162316109+Fislipe@users.noreply.github.com>
2026-02-17 22:19:18 -0300

    Add files via upload

---
+++ database/usuarios.py
@@ -0,0 +1,7 @@
+USUARIOS = [
+    {"id": 1, "nome": "joao", "email":"joaozinhogamer@gmail.com"},
+    {"id": 2, "nome": "carlos", "email":"carlos00@gmail.com"},
+    {"id": 3, "nome": "ellen", "email":"ellen@gmail.com"},
+    {"id": 4, "nome": "vitor", "email":"vitorzin@gmail.com"},
+    {"id": 5, "nome": "veloso", "email":"velosogameplays@gmail.com"}
+    ]   
